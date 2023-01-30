4043d6716e718859684178d8ff3140feab710db6
Joseph Smith <joesmith40@gmail.com>
2023-01-29 21:20:20 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+document.querySelector("main")
+main.remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Joseph is the champion"
+
+
+
+
+
+
+
+
+
+
